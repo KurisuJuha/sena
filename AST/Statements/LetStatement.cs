7894d3bad3e5c0d7d792d39f548cd6c4a84f9163
KYapp <kukiyomenaisapp@gmail.com>
2022-12-07 22:33:27 +0900

    Create LetStatement.cs

---
+++ AST/Statements/LetStatement.cs
@@ -0,0 +1,16 @@
+﻿using sena.AST.Expressions;
+
+namespace sena.AST.Statements
+{
+    public class LetStatement : IStatement
+    {
+        public readonly Identifier name;
+        public readonly IExpression value;
+
+        public LetStatement(Identifier name, IExpression value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+    }
+}
