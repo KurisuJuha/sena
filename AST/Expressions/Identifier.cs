fb5c26e4c8a334a0582f7bd100c74317b1b96f59
KYapp <kukiyomenaisapp@gmail.com>
2022-12-07 22:24:13 +0900

    IdentifierExpression

---
+++ AST/Expressions/Identifier.cs
@@ -0,0 +1,16 @@
+﻿using sena.Lexing;
+
+namespace sena.AST.Expressions
+{
+    public class Identifier : IExpression
+    {
+        public readonly Token token;
+        public readonly string value;
+
+        public Identifier(Token token, string value)
+        {
+            this.token = token;
+            this.value = value;
+        }
+    }
+}
