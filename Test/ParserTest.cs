b8a07823dd0300f740bde4c98d11146b084fb53d
KYapp <kukiyomenaisapp@gmail.com>
2022-12-07 22:59:11 +0900

    Letstatementのテスト

---
+++ Test/ParserTest.cs
@@ -0,0 +1,36 @@
+﻿using sena.AST;
+using sena.AST.Statements;
+using sena.Parsing;
+
+namespace sena.Test;
+
+public class ParserTest
+{
+    [Fact]
+    public void LetStatement1()
+    {
+        var code = @"
+let a = 3;
+let b = 12314314;
+let cfawfaw = 444444444;
+";
+        Lexer lexer = new Lexer(code);
+        Parser parser = new Parser(lexer);
+        Root root = parser.Parse();
+
+        Assert.Equal(3, root.statements.Count);
+
+        var names = new List<string>()
+        {
+            "a",
+            "b",
+            "cfawfaw"
+        };
+
+        for (int i = 0; i < 3; i++)
+        {
+            LetStatement statement = root.statements[i] as LetStatement;
+            string name = statement.name.value;
+        }
+    }
+}
