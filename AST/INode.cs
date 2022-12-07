e0ce6e24e215c3473d7521cd397f2fee8f39dee2
KYapp <kukiyomenaisapp@gmail.com>
2022-12-07 22:17:23 +0900

    Rootノードの実装

---
+++ AST/INode.cs
@@ -0,0 +1,7 @@
+﻿namespace sena.AST
+{
+	public interface INode
+	{
+
+	}
+}
---
+++ AST/Root.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.ObjectModel;
+
+namespace sena.AST
+{
+	public class Root : INode
+	{
+		public readonly ReadOnlyCollection<IStatement> statements;
+
+		public Root(List<IStatement> statements)
+		{
+			this.statements = statements.AsReadOnly();
+		}
+	}
+}
