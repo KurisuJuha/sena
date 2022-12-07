e4d59515908cb930dd2769ffc998018da68d1aca
KYapp <kukiyomenaisapp@gmail.com>
2022-12-07 22:50:44 +0900

    Update Parser.cs

--- Parser/Parser.cs
+++ Parser/Parser.cs
@@ -1,13 +1,30 @@
-﻿using sena.Lexing;
+﻿using sena.AST;
+using sena.Lexing;
 
 namespace sena.Parsing;
 
 public class Parser
 {
+    public Token currentToken { get; private set; }
+    public Token nextToken { get; private set; }
     Lexer lexer;
 
     public Parser(Lexer lexer)
     {
         this.lexer = lexer;
+
+        currentToken = lexer.NextToken();
+        nextToken = lexer.NextToken();
+    }
+
+    private void ReadToken()
+    {
+        currentToken = nextToken;
+        nextToken = lexer.NextToken();
+    }
+
+    public Root Parse()
+    {
+        return null;
     }
 }
