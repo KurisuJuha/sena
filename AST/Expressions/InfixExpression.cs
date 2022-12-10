4a26b40d3887245d42d3ba5ecee6b51a7fd17636
KYapp <kukiyomenaisapp@gmail.com>
2022-12-10 20:37:17 +0900

    Create InfixExpression.cs

---
+++ AST/Expressions/InfixExpression.cs
@@ -0,0 +1,20 @@
+﻿namespace sena.AST.Expressions;
+
+public class InfixExpression : IExpression
+{
+    public readonly string op;
+    public readonly IExpression rightExpression;
+    public readonly IExpression leftExpression;
+
+    public InfixExpression(string op, IExpression rightExpression, IExpression leftExpression)
+    {
+        this.op = op;
+        this.rightExpression = rightExpression;
+        this.leftExpression = leftExpression;
+    }
+
+    public string ToCode()
+    {
+        return $"( {rightExpression.ToCode()} {op} {leftExpression.ToCode()} )";
+    }
+}
