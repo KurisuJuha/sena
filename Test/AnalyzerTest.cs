91b5f7a13beecb7d224090178497c8f3b63b92e6
KYapp <kukiyomenaisapp@gmail.com>
2022-12-11 22:15:37 +0900

    AnalyzeLetStatementのテスト
    当然エラー

---
+++ Test/AnalyzerTest.cs
@@ -0,0 +1,27 @@
+﻿using sena.Parsing;
+using sena.SemanticsAnalysis;
+using Xunit.Abstractions;
+
+namespace sena.Test;
+
+public class AnalyzerTest
+{
+    private readonly ITestOutputHelper Console;
+    public AnalyzerTest(ITestOutputHelper testOutputHelper)
+    {
+        Console = testOutputHelper;
+    }
+
+    [Fact]
+    public void AnalyzeLetStatementTest()
+    {
+        string code = $"let a = 1;";
+
+        Lexer lexer = new Lexer(code);
+        Errors errors = new Errors();
+        Parser parser = new Parser(lexer, errors, Console.WriteLine);
+        SemanticsAnalyzer semanticsAnalyzer = new SemanticsAnalyzer(parser.Parse());
+
+        Assert.True(semanticsAnalyzer.Analyze());
+    }
+}
