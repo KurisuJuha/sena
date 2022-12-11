fc0a27030b8b7c0cb07360def00176356af1401e
KYapp <kukiyomenaisapp@gmail.com>
2022-12-11 22:11:02 +0900

    SemanticsAnalyzeを定義した

---
+++ SemanticAnalysis/SemanticsAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using sena.AST;
+
+namespace sena.SemanticsAnalysis;
+
+public class SemanticsAnalyzer
+{
+    readonly Root root;
+
+    public SemanticsAnalyzer(Root root)
+    {
+        this.root = root;
+    }
+
+    public bool Analyze()
+    {
+        return root.statements.All(AnalyzeStatement);
+    }
+
+    public bool AnalyzeStatement(IStatement statement)
+    {
+        switch (statement)
+        {
+            default:
+                return false;
+        }
+    }
+}
