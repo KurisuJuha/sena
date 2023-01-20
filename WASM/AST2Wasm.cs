6f6f2d113582560c9fc4cb1eaee596982e75b9d9
Juha <kukiyomenaisapp@gmail.com>
2023-01-20 19:29:02 +0900

    基盤作った

---
+++ WASM/AST2Wasm.cs
@@ -0,0 +1,24 @@
+﻿using sena.AST;
+
+namespace sena.Wasm;
+
+public class AST2Wasm
+{
+    readonly Root root;
+
+    public AST2Wasm(Root root)
+    {
+        this.root = root;
+    }
+
+    public string Compile() => CompileNode(root);
+
+    private string CompileNode(INode node)
+    {
+        switch (node)
+        {
+            default:
+                return "";
+        }
+    }
+}
